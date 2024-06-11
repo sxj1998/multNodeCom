d547f8c9eadfd01a9720c60aac5395ad3d7e61e3
shexingju <1970237065@qq.com>
2024-06-11 20:02:30 +0800

    增加contain of

---
+++ MultNodeCom/utils/contain_of.h
@@ -0,0 +1,20 @@
+#ifndef __CONTAIN_OF_H__
+#define __CONTAIN_OF_H__
+
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
+
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:	the pointer to the member.
+ * @type:	the type of the container struct this is embedded in.
+ * @member:	the name of the member within the struct.
+ *
+ */
+#define container_of(ptr, type, member) ({			\
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+	(type *)( (char *)__mptr - offsetof(type,member) );})
+
+
+#endif /* __CONTAIN_OF_H__ */
