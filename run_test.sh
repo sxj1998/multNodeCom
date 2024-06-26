f0a768289b8992ee3ce7fda826c3f79f2498b2de
shexingju <1970237065@qq.com>
2024-06-26 20:22:36 +0800

    输出带颜色

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 cd build
 cmake ..
 make
-./cppUtest_serial_start -g cppUtest_serial_local_rb -v
-./cppUtest_serial_start -g cppUtest_serial_local_hard_test -v
+./cppUtest_serial_start -g cppUtest_serial_local_rb -v -c
+./cppUtest_serial_start -g cppUtest_serial_local_hard_test -v -c
